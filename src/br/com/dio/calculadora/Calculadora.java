eb2120f1a3454219f2a11e85963acd76e6248bfd
matheusmotaandrelino <matheusmotaandrelino@gmail.com>
2021-09-21 23:29:01 -0300

    criando class Calculadora

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,43 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextInt();
+        System.out.println("Digite o Segundo valor: ");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int dividir = dividir(a, b);
+        int multiplicar = multiplicar(a, b);
+
+        System.out.println("somar: " + somar);
+        System.out.println("subt: " + subtrair);
+        System.out.println("div: " + dividir);
+        System.out.println("mult: " + multiplicar);
+
+    }
+
+    public static int somar(int a, int b ){
+        return a + b;
+    }
+
+    public static int subtrair(int a, int b ){
+        return a - b;
+    }
+
+    public static int dividir(int a, int b ){
+        return a / b;
+    }
+
+    public static int multiplicar(int a, int b ){
+        return a * b;
+    }
+}
